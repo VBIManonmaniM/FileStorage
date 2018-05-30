67c01f78d5891a2327fc3b3ae0a794f3fc0df182
Mano M <manonmanim@visualbi.com>
2018-05-30 22:41:27 +0530

    example js added

--- example.js
+++ example.js
@@ -4,68 +4,56 @@ const fs = require('fs');
 
 let storage = new StorageApi();
 
-// storage.open('resume.pdf','client1').then((result) => {
-//     result.stream.pipe(fs.createWriteStream('./localfiles/' + result.fileName));
-// }, (err) => {
-// });
-
-// storage.open('cli3.csv').then((result) => {
-//     result.stream.pipe(fs.createWriteStream('./localfiles/' + result.fileName));
-// }, (err) => {
-// });
-
-
-// storage.save('cli3.csv' , null , fs.createReadStream('./localfiles/cli3.csv')).then((result) => {
-// }, (err) => {
-// });
-
-// storage.save('resume.pdf' , 'resume' , fs.createReadStream('./localfiles/resume.pdf')).then((result) => {
-// }, (err) => {
-// });
-
-
-
-// storage.delete('cli3.csv').then((result) => {
-// }, (err) => {
-// });
-
-// storage.delete('resume.pdf' , 'resume').then((result) => {
-// }, (err) => {
-// });
-
-// storage.rename("resume.pdf", "rws.pdf").then((result) => {
-//     debugger
-// }, (err) => {
-//     debugger
-// });
-
-// storage.rename("resume.pdf", "rws.pdf","resume").then((result) => {
-//     debugger
-// }, (err) => {
-//     debugger
-// });
-
-
-// storage.getAttributes("res1.pdf").then((result) => {
-//     debugger
-// }, (err) => {
-//     debugger
-// });
-
-// storage.getAttributes("rws.pdf" , "resume").then((result) => {
-//     debugger
-// }, (err) => {
-//     debugger
-// });
-
-storage.removeDirectory("mano").then((result) => {
-    debugger
+storage.open('resume.pdf','client1').then((result) => {
+    result.stream.pipe(fs.createWriteStream('./localfiles/' + result.fileName));
 }, (err) => {
-    debugger
 });
 
-// storage.createDirectory("mano").then( (result) => {
-//     debugger
-// }, (err) => {
-//     debugger
-// });
+storage.open('cli3.csv').then((result) => {
+    result.stream.pipe(fs.createWriteStream('./localfiles/' + result.fileName));
+}, (err) => {
+});
+
+
+storage.save('cli3.csv' , null , fs.createReadStream('./localfiles/cli3.csv')).then((result) => {
+}, (err) => {
+});
+
+storage.save('resume.pdf' , 'resume' , fs.createReadStream('./localfiles/resume.pdf')).then((result) => {
+}, (err) => {
+});
+
+
+
+storage.delete('cli3.csv').then((result) => {
+}, (err) => {
+});
+
+storage.delete('resume.pdf' , 'resume').then((result) => {
+}, (err) => {
+});
+
+storage.rename("resume.pdf", "rws.pdf").then((result) => {
+}, (err) => {
+});
+
+storage.rename("resume.pdf", "rws.pdf","resume").then((result) => {
+}, (err) => {
+});
+
+
+storage.getAttributes("res1.pdf").then((result) => {
+}, (err) => {
+});
+
+storage.getAttributes("rws.pdf" , "resume").then((result) => {
+}, (err) => {
+});
+
+storage.removeDirectory("resume").then((result) => {
+}, (err) => {
+});
+
+storage.createDirectory("resume").then( (result) => {
+}, (err) => {
+});
